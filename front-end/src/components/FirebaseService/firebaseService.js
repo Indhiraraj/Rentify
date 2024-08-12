53ee6955799afc974e77b4f675b64384fafbc252
indhiraraj <indhiraraj7@gmail.com>
2024-08-12 20:58:43 +0530

    verify firebase bucket

--- front-end/src/components/FirebaseService/firebaseService.js
+++ front-end/src/components/FirebaseService/firebaseService.js
@@ -8,22 +8,19 @@ import {
 } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log("Firebase Storage Bucket:", process.env.REACT_APP_FIREBASE_STORAGE_BUCKET);
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
-console.log(process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
-console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID);
-
-
-
+console.log("Firebase Storage Bucket:", import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+console.log(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
+console.log(import.meta.env.VITE_FIREBASE_PROJECT_ID);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -53,4 +50,3 @@ export const deleteImageLink = async (id, name) => {
 };
 
 export { getImageLink };
-
