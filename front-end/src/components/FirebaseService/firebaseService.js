fc0b8ae0e0c130383386c1490c65f6222fe5dd81
indhiraraj <indhiraraj7@gmail.com>
2024-08-12 19:20:40 +0530

    env change

--- front-end/src/components/FirebaseService/firebaseService.js
+++ front-end/src/components/FirebaseService/firebaseService.js
@@ -4,35 +4,28 @@ import {
   ref,
   uploadBytes,
   getDownloadURL,
-  listAll,
+  deleteObject,
 } from "firebase/storage";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
-// Read the file from the filesystem
-
 const getImageLink = async (id, name, file) => {
   const storageRef = ref(storage, `${id}/${name}.png`);
 
   try {
-
     await uploadBytes(storageRef, file);
-
     const url = await getDownloadURL(storageRef);
     return url;
   } catch (error) {
@@ -40,17 +33,16 @@ const getImageLink = async (id, name, file) => {
   }
 };
 
-export const deleteImageLink = async (id, name, file) => {
+export const deleteImageLink = async (id, name) => {
   const storageRef = ref(storage, `${id}/${name}.png`);
 
-  // Delete the file
-  deleteObject(storageRef)
-    .then(() => {
-      return true;
-    })
-    .catch((error) => {
-      return false;
-    });
+  try {
+    await deleteObject(storageRef);
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 
 export { getImageLink };
+
