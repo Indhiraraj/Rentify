5ab8f3deeea287377ad8889dcda1627da1873501
indhiraraj <indhiraraj7@gmail.com>
2024-08-13 09:42:13 +0530

    add click fuctionality to wishlist and popular property

--- front-end/src/components/PopularProperties/PropertyCard/PropertyCard.jsx
+++ front-end/src/components/PopularProperties/PropertyCard/PropertyCard.jsx
@@ -13,41 +13,52 @@ const PropertyCard = ({area}) => {
     }
   },[wishlist])
 
- 
-
-  const handleAddWishlist = async () => {
+  const handleAddWishlist = async (event) => {
+    event.stopPropagation();
     setFavourite(!favourite);
-    const response = await fetch(`https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,{
+    const response = await fetch(
+      `https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,
+      {
         method: "POST",
-        headers: {"content-type" : "application/json"},
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({
-            area : area.areaId
-        })
-    });
+          area: area.areaId,
+        }),
+      }
+    );
     await fetchWishlist();
+  };
 
-}
-
-const handleRemoveWishlist = async () => {
+  const handleRemoveWishlist = async (event) => {
+    event.stopPropagation();
     setFavourite(!favourite);
-    const response = await fetch(`https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,{
+    const response = await fetch(
+      `https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,
+      {
         method: "DELETE",
-        headers: {"content-type" : "application/json"},
+        headers: { "content-type": "application/json" },
         body: JSON.stringify({
-            area : area.areaId
-        })
-    });
+          area: area.areaId,
+        }),
+      }
+    );
     await fetchWishlist();
-    
-}
+  };
+
   return (
-    <div className="property-card">
+    <div className="property-card" onClick={() => navigate(`/areas/${area.areaId}`)}>
       <div className="img-icon">
-        {favourite ? (
-          <Favorite onClick={() => handleRemoveWishlist()} className="fav-icon fav-icon-full" />
-        ) : (
-          <Favorite onClick={() => handleAddWishlist()} className="fav-icon fav-icon-border" />
-        )}
+      {favourite ? (
+            <Favorite
+              onClick={handleRemoveWishlist}
+              className="fav-icon fav-icon-full"
+            />
+          ) : (
+            <Favorite
+              onClick={handleAddWishlist}
+              className="fav-icon fav-icon-border"
+            />
+          )}
         <img src={area.images[0]} alt="img" />
       </div>
 
--- front-end/src/components/WishListCard/WishListCard.jsx
+++ front-end/src/components/WishListCard/WishListCard.jsx
@@ -7,39 +7,54 @@ const WishListCard = ({area}) => {
     const [favourite,setFavourite] = useState(area.iswishlistItem);
     const {user,fetchWishlist} = useContext(RentifyContext);
 
-    const handleAddWishlist = async () => {
-        
-        const response = await fetch(`https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,{
-            method: "POST",
-            headers: {"content-type" : "application/json"},
-            body: JSON.stringify({
-                area : area.areaId
-            })
-        });
-        
-        await fetchWishlist();
-    }
-    
-    const handleRemoveWishlist = async () => {
-        
-        const response = await fetch(`https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,{
-            method: "DELETE",
-            headers: {"content-type" : "application/json"},
-            body: JSON.stringify({
-                area : area.areaId
-            })
-        });
-        await fetchWishlist();
-    }
+    const handleAddWishlist = async (event) => {
+      event.stopPropagation();
+      setFavourite(!favourite);
+      const response = await fetch(
+        `https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,
+        {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify({
+            area: area.areaId,
+          }),
+        }
+      );
+      await fetchWishlist();
+    };
+  
+    const handleRemoveWishlist = async (event) => {
+      event.stopPropagation();
+      setFavourite(!favourite);
+      const response = await fetch(
+        `https://rentify-backend-olive.vercel.app/api/wishlist/${user.userId}`,
+        {
+          method: "DELETE",
+          headers: { "content-type": "application/json" },
+          body: JSON.stringify({
+            area: area.areaId,
+          }),
+        }
+      );
+      await fetchWishlist();
+    };
+  
 
   return (
-    <div className="wishlist-card">
+    <div className="wishlist-card" onClick={() => navigate(`/areas/${area.areaId}`)}>
       <div className="img-icon">
-        {favourite ? (
-          <Favorite onClick={() => handleRemoveWishlist()} className="fav-icon fav-icon-full" />
-        ) : (
-          <Favorite onClick={() => handleAddWishlist()} className="fav-icon fav-icon-border" />
-        )}
+      {favourite ? (
+            <Favorite
+              onClick={handleRemoveWishlist}
+              className="fav-icon fav-icon-full"
+            />
+          ) : (
+            <Favorite
+              onClick={handleAddWishlist}
+              className="fav-icon fav-icon-border"
+            />
+          )}
+
         <img src={area.images[0]} alt={area.area_details.title} />
       </div>
 
